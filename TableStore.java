f55b081db242d97ff49b1701ed4fbab64bd72d1b
TheBlueFish116 <48139732+TheBlueFish116@users.noreply.github.com>
2019-10-10 10:26:59 -0600

    Add files via upload

---
+++ TableStore.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esof.pkg2;
+
+/**
+ *
+ * @author dylan
+ */
+public class TableStore implements Storetype {
+    public void store(){
+        System.out.println("Table Store");
+    }
+}
